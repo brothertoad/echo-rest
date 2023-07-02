cd775edaa1389e5cd9f7be0052678a7178d90c30
Stacey Brogden <toad@brothertoad.net>
2023-07-01 21:25:35 -0700

    Remove kind from model and db table.

--- main.go
+++ main.go
@@ -18,8 +18,8 @@ func main() {
 	defer db.Close()
 
 	e := echo.New()
-	e.GET("/block/:kind/:name", func(c echo.Context) error {
-		return getRawBlock(c, db)
+	e.GET("/block/:name", func(c echo.Context) error {
+		return getBlock(c, db)
 	})
 	e.POST("/block", func(c echo.Context) error {
 		return postBlock(c, db)
@@ -27,18 +27,18 @@ func main() {
 	e.Logger.Fatal(e.Start(":9903"))
 }
 
-func getRawBlock(c echo.Context, db *sql.DB) error {
+func getBlock(c echo.Context, db *sql.DB) error {
 	block := new(model.BlockRequest)
 	if err := c.Bind(block); err != nil {
 		return err
 	}
-	err := db.QueryRow("select contents, modTime from blocks where name = $1 and kind = $2", block.Name, block.Kind).Scan(&block.Contents, &block.ModTime)
+	err := db.QueryRow("select contents, modTime from blocks where name = $1 order by modTime desc limit 1", block.Name).Scan(&block.Contents, &block.ModTime)
 	if err != nil {
 		// If there was an error, just assume we are creating a new block, so just return an empty one.
 		block.Contents = ""
 		block.ModTime = time.Now()
 	}
-	fmt.Printf("getRawBlock: returning %+v\n", block)
+	fmt.Printf("getBlock: returning %+v\n", block)
 	return c.JSON(http.StatusOK,  block)
 }
 
@@ -47,7 +47,7 @@ func postBlock(c echo.Context, db *sql.DB) error {
 	if err := c.Bind(block); err != nil {
 		return err
 	}
-	result, err := db.Exec("insert into blocks (name, kind, contents, modTime) values ($1, $2, $3, $4)", block.Name, block.Kind, block.Contents, time.Now())
+	result, err := db.Exec("insert into blocks (name, contents, modTime) values ($1, $2, $3)", block.Name, block.Contents, time.Now())
 	if err != nil {
 		return c.String(http.StatusBadRequest, err.Error())
 	}
@@ -58,5 +58,6 @@ func postBlock(c echo.Context, db *sql.DB) error {
 	if numRows != 1 {
 		// should not happen
 	}
+	fmt.Printf("postBlock: status OK\n")
 	return c.String(http.StatusOK, "")
 }
--- model/block.go
+++ model/block.go
@@ -6,7 +6,6 @@ import (
 
 type BlockRequest struct {
   Name string `json:"name" form:"name" param:"name"`
-  Kind string `json:"kind" form:"kind" param:"kind"`
   Contents string `json:"contents" form:"contents" param:"contents"`
   ModTime time.Time `json:"modTime" form:"modTime" param:"modTime"`
 }
