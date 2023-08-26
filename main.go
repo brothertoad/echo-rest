9856b49dc395a255b5f64faab8c1fe858e924789
Stacey Brogden <toad@brothertoad.net>
2023-08-25 19:33:26 -0700

    Another checkpoint for adding cli.

--- main.go
+++ main.go
@@ -2,16 +2,15 @@ package main
 
 import (
 	"database/sql"
-	"net/http"
+	"fmt"
 	"time"
-	"github.com/labstack/echo/v4"
 )
 
 func main() {
 	doServe()
 }
 
-func fetchBlock(db *sql.DB, block *BlockRequest) error {
+func getBlock(db *sql.DB, block *BlockRequest) error {
 	err := db.QueryRow("select contents, modTime from blocks where name = $1 order by modTime desc limit 1", block.Name).Scan(&block.Contents, &block.ModTime)
 	if err != nil {
 		// If there was an error, just assume we are creating a new block, so just return an empty one.
@@ -21,35 +20,18 @@ func fetchBlock(db *sql.DB, block *BlockRequest) error {
 	return nil
 }
 
-func getBlock(c echo.Context, db *sql.DB) error {
-	block := new(BlockRequest)
-	if err := c.Bind(block); err != nil {
-		return err
-	}
-	err := db.QueryRow("select contents, modTime from blocks where name = $1 order by modTime desc limit 1", block.Name).Scan(&block.Contents, &block.ModTime)
-	if err != nil {
-		// If there was an error, just assume we are creating a new block, so just return an empty one.
-		block.Contents = ""
-		block.ModTime = time.Now()
-	}
-	return c.JSON(http.StatusOK,  block)
-}
-
-func postBlock(c echo.Context, db *sql.DB) error {
-	block := new(BlockRequest)
-	if err := c.Bind(block); err != nil {
-		return err
-	}
+func putBlock(db *sql.DB, block *BlockRequest) error {
 	result, err := db.Exec("insert into blocks (name, contents, modTime) values ($1, $2, $3)", block.Name, block.Contents, time.Now())
 	if err != nil {
-		return c.String(http.StatusBadRequest, err.Error())
+		return err
 	}
 	numRows, err := result.RowsAffected()
 	if err != nil {
-		return c.String(http.StatusBadRequest, err.Error())
+		return err
 	}
+	// should not happen, but we'll check for it anyway
 	if numRows != 1 {
-		// should not happen
+		return fmt.Errorf("More than one row (%d) affected by insert\n", numRows)
 	}
-	return c.String(http.StatusOK, "")
+	return nil
 }
--- serve.go
+++ serve.go
@@ -2,6 +2,7 @@ package main
 
 import (
   "database/sql"
+  "net/http"
   "os"
   "github.com/labstack/echo/v4"
   "github.com/labstack/echo/v4/middleware"
@@ -17,12 +18,33 @@ func doServe() {
 	e.Use(middleware.Logger())
 	e.Use(middleware.CORS()) // allow all requests
 	e.GET("/block/:name", func(c echo.Context) error {
-		return getBlock(c, db)
+		return getBlockForREST(c, db)
 	})
 	e.POST("/block", func(c echo.Context) error {
-		return postBlock(c, db)
+		return putBlockForREST(c, db)
 	})
 	// Should specify port in some kind of configuration.
 	e.Logger.Fatal(e.Start(":9903"))
   // return nil
+}
+
+func getBlockForREST(c echo.Context, db *sql.DB) error {
+	block := new(BlockRequest)
+	if err := c.Bind(block); err != nil {
+		return err
+	}
+  getBlock(db, block)
+	return c.JSON(http.StatusOK, block)
+}
+
+func putBlockForREST(c echo.Context, db *sql.DB) error {
+	block := new(BlockRequest)
+	if err := c.Bind(block); err != nil {
+		return err
+	}
+  err := putBlock(db, block)
+  if err != nil {
+    return c.String(http.StatusInternalServerError, err.Error())
+  }
+	return c.String(http.StatusOK, "")
 }
